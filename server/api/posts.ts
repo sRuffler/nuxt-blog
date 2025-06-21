09a17fae69ee196ce80af3196bc04e96896e024e
stefan_ruffler <s.ruffler@mmr-research.com>
2025-06-21 11:50:35 +0100

    prisma update

--- server/api/posts.ts
+++ server/api/posts.ts
@@ -1,5 +1,4 @@
-import { PrismaClient } from '@prisma/client';
-const prisma = new PrismaClient();
+import prisma from "../utils/prisma"
 
 export default defineEventHandler(async (event) => {
   if (event.method === 'POST') {
